435087d309a209fe49088d190d7fdf44aaa42b20
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 10:12:46 +0200

    patch 2

--- script/code.js
+++ script/code.js
@@ -9,7 +9,7 @@ let base = +prompt('Enter Base Value')
 let height = +prompt('Enter Height Value')
 let area = (base * height) * 0.5 
 
-//loging results
+//logging results
 console.log(`The base of the triangle is ${base} and the height is ${height}. Your area of triangle is ${area}`)
 
 }
