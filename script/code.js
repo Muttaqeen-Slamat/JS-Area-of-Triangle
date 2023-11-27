e266beb0865801ac74298329a5e896beeb71fa75
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 10:10:30 +0200

    patch

---
+++ script/code.js
@@ -0,0 +1,18 @@
+//creating function
+function area(){
+
+//calculating area of triangle
+
+// formula is base x height x 1/2 or 0.5
+
+let base = +prompt('Enter Base Value')
+let height = +prompt('Enter Height Value')
+let area = (base * height) * 0.5 
+
+//loging results
+console.log(`The base of the triangle is ${base} and the height is ${height}. Your area of triangle is ${area}`)
+
+}
+area()
+
+
